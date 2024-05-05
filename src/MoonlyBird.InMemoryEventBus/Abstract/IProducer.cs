9d6a7d4ed0328f2a97e44d9df5e3bfeebc06b756
m3mbrillo <wasinger.facundo@gmail.com>
2024-05-05 02:24:51 -0300

    Fix recursive call IProducer

--- src/MoonlyBird.InMemoryEventBus/Abstract/IProducer.cs
+++ src/MoonlyBird.InMemoryEventBus/Abstract/IProducer.cs
@@ -12,5 +12,5 @@ public interface IProducer<T> : IProducer
     ValueTask PublishAsync(Event<T> @event, CancellationToken token = default);
 
     ValueTask IProducer.PublishAsync(Event<object> @event, CancellationToken token)
-        => PublishAsync(@event, token);
+        => PublishAsync((@event as Event<T>) ?? throw new ArgumentException("Invalid type. Expected type: " + typeof(Event<T>).FullName), token);
 }
